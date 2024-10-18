d45357b07b2d9b1b6617569fd3139728d530ec3b
Timothy <timlundy74@gmail.com>
2024-10-18 15:22:06 -0400

    Ice Cream assignment fixed.

--- auto/script.js
+++ auto/script.js
@@ -4,27 +4,34 @@
 // const gpsButton = document.getElementById("gpsButton")
 // const roadsideButton = document.getElementById("roadsideButton")
 
-let displayChangeBtn = document.getElementById("displayChange")
-let estimationDisplay = document.getElementById("estimation")
-let showMore = document.getElementById("hide/show")
+let displayChangeBtn = document.getElementById("displayChange"); //calling line 57
+let estimationDisplay = document.getElementById("estimation"); // calling line 40
+let showMore = document.getElementById("hide/show");
 
 function estimateCost() {
+  ///////////////////Buttons that check\\\\\\\\\\\\\\\\\\\\\
   let tollButton = document.getElementById("tollButton");
   let gpsButton = document.getElementById("gpsButton");
   let roadsideButton = document.getElementById("roadsideButton");
+  //////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+  ///////////////////////////////Fields for money and such!\\\\\\\\\\\\\\\\\\\\\\\\
   let numberOfDaysInput = document.getElementById("numberOfDaysInput");
   const amountDue = document.getElementById("amountDue");
   const basePayField = document.getElementById("basepay");
-  const optionField = document.getElementById("options")
-  const under25Field = document.getElementById("under25Surcharge")
+  const optionField = document.getElementById("options");
+  const under25Field = document.getElementById("under25Surcharge");
+  ////////////////////////////////////////////////////////////////////////
+
+  /////////////////////Radio button\\\\\\\\\\\\\\\\\\\\\\\\\\
 
   let under25 = document.getElementById("25selectYes");
+  ///////////////////////////////////////////////////////////////////////
 
   let days = Number(numberOfDaysInput.value);
 
-//   let displayChange = document.getElementById("displayChange")
-//   let estimationDisplay = document.getElementById("estimation")
+  //   let displayChange = document.getElementById("displayChange")
+  //   let estimationDisplay = document.getElementById("estimation")
 
   const baseRate = 29.99;
   const tollRate = 3.95;
@@ -34,64 +41,60 @@ function estimateCost() {
 
   let rentalAmount = baseRate * days;
   let optionAmount = 0;
-  let under25Age = 0
+  let under25Age = 0;
 
   if (tollButton.checked) {
-    let sum = tollRate * days
-    optionAmount = optionAmount + sum
-// basePayField.value = sum
-    // console.log(sum)
-    // amountDue.value = sum;
-  } 
-
-  if(gpsButton.checked){
-    let sum = gpsRate * days
-    optionAmount = optionAmount + sum
+    let sum = tollRate * days;
+    optionAmount = optionAmount + sum;
+    // basePayField.value = sum
     // console.log(sum)
     // amountDue.value = sum;
   }
 
-  if(roadsideButton.checked){
-    let sum = roadsideRate * days
-    optionAmount = optionAmount + sum
+  if (gpsButton.checked) {
+    let sum = gpsRate * days;
+    optionAmount = optionAmount + sum;
     // console.log(sum)
     // amountDue.value = sum;
-
   }
 
-  if(under25.checked){
+  if (roadsideButton.checked) {
+    let sum = roadsideRate * days;
+    optionAmount = optionAmount + sum;
+    // console.log(sum)
+    // amountDue.value = sum;
+  }
+
+  if (under25.checked) {
     let under25Amount = baseRate * under25Surcharge;
     under25Age = under25Amount;
     // under25Surcharge = Number(under25Amount.value);
   }
 
+  //   let under25Amount = baseRate * under25Surcharge;
+  let totalAmount = rentalAmount + optionAmount + under25Age;
 
-  
-//   let under25Amount = baseRate * under25Surcharge;
-  let totalAmount = rentalAmount + optionAmount + under25Age
-
-amountDue.value = Number(totalAmount.toFixed(2))
+  amountDue.value = Number(totalAmount.toFixed(2)); //Shows the total amount on the amountDue field called on line 54 in HTML & line 20 in JavaScript(JS)
   console.log(totalAmount);
 
- basePayField.value = rentalAmount.toFixed(2)
-console.log(basePayField)
+  basePayField.value = rentalAmount.toFixed(2); //Shows baseRate times the days in the input basePay field/
+  console.log(basePayField);
 
-optionField.value = Number(optionAmount.toFixed(2))
-console.log(optionField)
+  optionField.value = Number(optionAmount.toFixed(2)); //Show the options checked amount total in the options field on line 47 in HTML and line 22 in JavaScript(JS)
+  console.log(optionField);
 
-under25Field.value = Number(under25Age.toFixed(2))
-console.log(under25Field)
-
+  under25Field.value = Number(under25Age.toFixed(2));
+  console.log(under25Field);
 }
 
-function displayChange(){
-        if(displayChangeBtn.checked){
-    estimationDisplay.style.display = "none"
-    showMore.innerText = "Show"
-        }else{
-estimationDisplay.style.display = "block"
-showMore.innerText = "Hide"
-        }
-    
-   
+/////////////To change the display of the estimation div\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function displayChange() {
+  if (displayChangeBtn.checked) {
+    estimationDisplay.style.display = "none";
+    showMore.innerText = "Show";
+  } else {
+    estimationDisplay.style.display = "block";
+    showMore.innerText = "Hide";
+  }
 }
+////////////////////////////////////////////////////////////////////////////
--- icecream/script.js
+++ icecream/script.js
@@ -1,93 +1,88 @@
-const scoopAmountField = document.getElementById("amountOfScoops")
-const coneButton = document.getElementById("coneButton")
-const cupButton = document.getElementById('cupButton')
-const sprinklesButton = document.getElementById("sprinklesButton")
-const creamButton = document.getElementById("creamButton")
-const fudgeButton = document.getElementById("fudgeButton")
-const cherryButton = document.getElementById("cherryButton")
+const scoopAmountField = document.getElementById("amountOfScoops");
+const coneButton = document.getElementById("coneButton");
+const cupButton = document.getElementById("cupButton");
+const sprinklesButton = document.getElementById("sprinklesButton");
+const creamButton = document.getElementById("creamButton");
+const fudgeButton = document.getElementById("fudgeButton");
+const cherryButton = document.getElementById("cherryButton");
 
 const toppingSelector = document.getElementById("toppingSelector");
 
-let basePriceField = document.getElementById("basePriceField")
-let taxField = document.getElementById("taxField")
-let totalAmountField = document.getElementById("totalAmountField")
+let basePriceField = document.getElementById("basePriceField");
+let taxField = document.getElementById("taxField");
+let totalAmountField = document.getElementById("totalAmountField");
 
-let displayAmounts = document.getElementById("displayAmounts") 
-let displayCheck = document.getElementById("displayCheck")
-let displayLabelCheck = document.getElementById("displayCheckLabel")
+let displayAmounts = document.getElementById("displayAmounts");
+let displayCheck = document.getElementById("displayCheck");
+let displayLabelCheck = document.getElementById("displayCheckLabel");
 
-let baseAmount = 2.25;
-let additionalScoops = 1.25;
-let sprinkles = .50;
-let whippedCreams = .25;
+const firstScoopCost = 2.25;
+let additionalScoopCost = 1.25;
+let sprinkles = 0.5;
+let whippedCreams = 0.25;
 let fudges = 1.25;
-let cherrys = .25;
+let cherrys = 0.25;
 
-const taxRate = 8.875
+const taxRate = 0.0975;
 
-function displayToppings(){
-    
-    if(cupButton.checked){
-        toppingSelector.style.display = "block"
-    }
-    
-    if(coneButton.checked){
-        toppingSelector.style.display = "none"
-    }
+function displayToppings() {
+  if (cupButton.checked) {
+    toppingSelector.style.display = "block";
+  }
+
+  if (coneButton.checked) {
+    toppingSelector.style.display = "none";
+  }
 }
 
-function displayAmount(){
-    if(displayCheck.checked){
+function displayAmount() {
+  if (displayCheck.checked) {
     displayAmounts.style.display = "block";
-    displayLabelCheck.innerText = "Hide"
-    }else{
-        displayAmounts.style.display = "none"
-        displayLabelCheck.innerText = "Show"
-    }
+    displayLabelCheck.innerText = "Hide";
+  } else {
+    displayAmounts.style.display = "none";
+    displayLabelCheck.innerText = "Show";
+  }
 }
 
-function calculate(){
+function calculate() {
+  let numberOfScoops = Number(scoopAmountField.value);
+  let numberOfAdditionalScoops = numberOfScoops -1
 
+  let basePriceAmount = firstScoopCost * 1 + (numberOfAdditionalScoops * additionalScoopCost);
+  
+  let totalToppingsCost = 0;
 
-    let scoopAmountInput =  Number(scoopAmountField.value)
-        
-    let applyAddition = baseAmount * scoopAmountInput * taxRate;
-    let Sum = 0;
+//   if (numberOfScoops > 1) {
+//     basePriceAmount = firstScoopCost + additionalScoopCost ;
+//   } else {
+//     basePriceAmount = firstScoopCost;
+//   }
 
-    if(scoopAmountInput > 1){
-applyAddition = baseAmount + additionalScoops * scoopAmountInput;
-    }else{
-     applyAddition = baseAmount
-    }
+  if (sprinklesButton.checked) {
+     totalToppingsCost = totalToppingsCost + sprinkles;
 
-    if(sprinklesButton.checked){
-    let add = Sum + sprinkles;
-    Sum = add;
-    }
+  }
 
-    if(creamButton.checked){
-        let add = Sum + whippedCreams;
-        Sum = add;
-    }
-
-    if(fudgeButton.checked){
-        let add = Sum + fudges;
-        Sum = add
-    }
-
-    if(cherryButton.checked){
-        let add = Sum + cherrys;
-        Sum = add;
-    }
-
-    let totalSum = applyAddition + Sum * taxRate
-
-    basePriceField.value = applyAddition.toFixed(2)
-
-    taxField.value = taxRate.toFixed(2)
-
-    totalAmountField.value = totalSum.toFixed(2)
-
+  if (creamButton.checked) {
+totalToppingsCost = totalToppingsCost + whippedCreams;
     
+  }
 
+  if (fudgeButton.checked) {
+ totalToppingsCost = totalToppingsCost + fudges;
+    
+  }
+
+  if (cherryButton.checked) {
+ totalToppingsCost = totalToppingsCost + cherrys;
+  }
+
+  let totalSum = basePriceAmount + totalToppingsCost * taxRate;
+
+  basePriceField.value = basePriceAmount.toFixed(2);
+
+  taxField.value = taxRate.toFixed(2);
+
+  totalAmountField.value = totalSum.toFixed(2);
 }
